5ca283202109afc57c0bbf740b4d535fe85446d3
Fenton K <fentonjkrupp1@gmail.com>
2024-10-04 17:19:24 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/fenton_headshot.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <>
+          <div>
+              <a href="https://vitejs.dev" target="_blank">
+              </a>
+              <a href="https://cat-bounce.com/" target="_blank">
+                  <img src={reactLogo} className="logo react" alt="React logo"/>
+              </a>
+          </div>
+          <h1>Fenton Krupp</h1>
+          <p className="read-the-docs">
+              web designer, writer, and IT professional
+          </p>
+      </>
+  )
+}
+
+export default App
