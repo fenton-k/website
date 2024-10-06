477fcb54478158495c3678a90b403f23457883d8
Fenton K <fentonjkrupp1@gmail.com>
2024-10-06 15:35:02 -0400

    big updates.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/fenton_headshot.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
       <>
